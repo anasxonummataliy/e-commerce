2245b849ef0a6cb382b3ec11018705793ba18afe
Anasxon Ummataliyev <aummataliy@gmail.com>
2026-03-01 17:54:44 +0500

    fix

--- app/database/models/category.py
+++ app/database/models/category.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database.base import TimeBaseModel
 
 if TYPE_CHECKING:
-    from category import Product
+    from product import Product
 
 
 class Category(TimeBaseModel):
