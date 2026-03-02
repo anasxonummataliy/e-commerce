b154470ee85863d855fad665adbe5748a43b2b06
Anasxon Ummataliyev <aummataliy@gmail.com>
2026-03-02 15:53:59 +0500

    fix

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,8 +1,7 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, Query
 
-from app.schemas import UserRegister, QParams
-from app.schemas.auth import TokenResponse
+
 from app.services.auth_service import AuthService
 
 
@@ -11,15 +10,15 @@ auth_router = APIRouter(prefix="/auth", tags=["Auth"])
 AuthServiceDep = Annotated[AuthService, Depends()]
 
 
-@auth_router.post("/register")
-async def register(
-    user_in: UserRegister, auth_service: AuthServiceDep
-) -> TokenResponse:
-    """Ro'yxatdan o'tish endpointi"""
-    return await auth_service.register_user(user_in)
+# @auth_router.post("/register")
+# async def register(
+#     user_in: UserRegister, auth_service: AuthServiceDep
+# ) -> TokenResponse:
+#     """Ro'yxatdan o'tish endpointi"""
+#     return await auth_service.register_user(user_in)
 
 
-@auth_router.get("")
-async def login(params: Annotated[QParams, Query()]):
-    """Kirish endpointi"""
-    return {"message": "Login endpoint - ishlayapti"}
+# @auth_router.get("")
+# async def login(params: Annotated[QParams, Query()]):
+#     """Kirish endpointi"""
+#     return {"message": "Login endpoint - ishlayapti"}
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,1 +1,14 @@
-from .auth import UserRegister
+from .auth import Token, TokenPayload
+from .product import CategoryResponse, ProductResponse
+from .cart import CartResponse, CartItemResponse, CartItemUpdate, CartItemAdd
+from .order import (
+    OrderResponse,
+    OrderItemResponse,
+    PaymentResponse,
+    OrderStatus,
+    OrderCreate,
+    PaymentStatus,
+)
+
+from .product import ProductCreate, ProductResponse, ProductUpdate
+from .user import AuthProvider, UserCreate, UserResponse, UserUpdate
